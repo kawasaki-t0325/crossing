4d415e8f2433e02dce7e3e12f54eb67274e15c59
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-10-13 20:54:06 +0900

    a8でpuppeteerからデータを取得する

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const puppeteer = require('puppeteer');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const runtimeOpts = {
+  memory: '1GB',
+};
+
+exports.searchItem = functions.runWith(runtimeOpts).https.onRequest(async (request, response) => {
+  const { username, password, word, selector } = request.body;
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+
+  await page.goto('https://www.a8.net/');
+  await page.type('input[name="login"]', username);
+  await page.type('input[name="passwd"]', password);
+  await page.click('input[name="lgin_as_btn"]');
+  await page.waitForNavigation();
+
+  await page.type('#headerSearchKeyword', word);
+  await page.click('input[type="submit"]');
+
+  const item = await page.$(selector).textContent;
+
+  const result = (item === '') ? '該当なし' : '見つかりました';
+
+  return response.status(200).send(result);
+});
