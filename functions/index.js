c282a5fb2e654a85b4db392cc783d203bbd20bfe
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-11-28 22:48:31 +0900

    cross originの問題を解消

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 
 const searchItem = require('./src/searchItem');
 
@@ -10,6 +11,8 @@ const runtimeOpts = {
 };
 
 exports.searchItem = functions.region('asia-northeast1').runWith(runtimeOpts).https.onRequest(async (request, response) => {
-  const result = await searchItem(request.body).catch(({ error, code }) => response.json({ code: code, message: error }));
-  response.json({ code: 200, ...result });
+  cors(request, response, async () => {
+    const result = await searchItem(request.body).catch(({ error, code }) => response.json({ code: code, message: error }));
+    response.json({ code: 200, ...result });
+  });
 });
