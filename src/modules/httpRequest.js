aad8f406cbd04dc74c2fc8c33b23fd5027bfc175
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-11-30 16:16:20 +0900

    responseが返ってこなかったときのためaxiosにtimeoutを設定

--- src/modules/httpRequest.js
+++ src/modules/httpRequest.js
@@ -5,7 +5,8 @@ class HttpRequest {
   static httpRequest = async (siteId, word, username, password) => {
     if (!siteId || !word || !username || !password) return {};
     try {
-      const result = await axios.post(`${process.env.REACT_APP_HOST}/crossing-39d56/asia-northeast1/searchItem`, {
+      const instance = axios.create({ timeout: 15000 });
+      const result = await instance.post(`${process.env.REACT_APP_HOST}/crossing-39d56/asia-northeast1/searchItem`, {
         site: siteId,
         word: word,
         username: username,
