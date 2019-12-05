fab87cdf3091aaa8ce18686ad2c087420e424e0e
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-12-06 08:55:50 +0900

    loadingのアニメーション画面を追加

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,6 +5,16 @@ import {
 } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
+  '@keyframes turn': {
+    from :{
+      transform: 'rotateX(0deg)',
+      transformOrigin: '0 2vw',
+    },
+    to :{
+      transform: 'rotateX(360deg)',
+      transformOrigin: '0 2vw',
+    },
+  },
   container: {
     position: 'fixed',
     maxWidth: 'initial',
@@ -17,23 +27,49 @@ const useStyles = makeStyles(theme => ({
     height: '100vh',
     width: '100vw',
     backgroundColor: '#000',
-    opacity: 0.7,
+    opacity: 0.9,
     zIndex: 1,
   },
   flex: {
     display: 'flex',
     justifyContent: 'center',
   },
-  text: {
+  border: {
+    position: 'relative',
+    height: '100%',
+    width: '100%'
+  },
+  textWrapper: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translateY(-50%) translateX(-50%)',
     color: '#fff',
   },
+  text: {
+    margin: theme.spacing(1),
+    animation: '$turn 3s infinite',
+  },
+  warning: {
+    marginTop: theme.spacing(3),
+  }
 }));
 
 function Loading() {
   const classes = useStyles();
+
   return (
     <Container className={`${classes.container} ${classes.flex}`}>
-      <p className={classes.text}>test</p>
+      <div className={classes.border}>
+        <div className={classes.textWrapper}>
+          <div className={classes.flex} style={{ borderBottom: '2px solid #fff', }}>
+            {'LOADING…'.split('').map((text, index) => {
+              return <div key={index} className={classes.text} style={{ animationDelay: `${index / 10}s`, }}>{text}</div>;
+            })}
+          </div>
+          <p className={classes.warning}>※検索には15秒前後の時間がかかります</p>
+        </div>
+      </div>
     </Container>
   );
 }
