8847d214b5a684b1357883cc844acda5ba3d1e58
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-12-30 16:25:54 +0900

    レイアウトの微修正

--- src/App.js
+++ src/App.js
@@ -48,15 +48,16 @@ const useStyles = makeStyles(theme => ({
     alignItems: 'center',
   },
   title_wrapper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(24),
   },
   title: {
     fontFamily: 'Georgia-BoldItalic',
   },
   main: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(2),
   },
   button: {
+    margin: theme.spacing(2),
     padding: theme.spacing(2, 4),
   },
   relative: {
@@ -68,8 +69,11 @@ const useStyles = makeStyles(theme => ({
     marginLeft: -12,
   },
   textWarning: {
-    color: '#d50000'
-  }
+    color: '#d50000',
+  },
+  footer: {
+    marginTop: theme.spacing(8),
+  },
 }));
 
 function App() {
@@ -135,6 +139,10 @@ function App() {
       && (result.afbResult['code'] !== RESPONSE_STATUS.SUCCESS);
   };
 
+  const isResultEmpty = () => {
+    return Object.values(result).filter(projectResult => Object.keys(projectResult).length !== 0).length === 0;
+  };
+
   return (
     <React.Fragment>
       {loadging && <Loading />}
@@ -143,7 +151,27 @@ function App() {
       </Container>
       <Container className={classes.main} component="main">
         <Grid container spacing={5} justify="center">
-          {asps.map(asp => (
+          <Container className={classes.container} maxWidth="md">
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              autoFocus
+              placeholder="検索商品"
+              value={word}
+              onChange={event => setWord(event.target.value)}
+            />
+            <Button className={classes.button} disabled={loadging} variant="contained" color="primary" onClick={search}>
+              検索する
+            </Button>
+          </Container>
+          <Grid item xs={12}>
+            {message && <Typography align="center" className={classes.textWarning}>{message}</Typography>}
+          </Grid>
+        </Grid>
+        <Grid container spacing={5} justify="center">
+          {!isResultEmpty() && asps.map(asp => (
             <Grid item key={asp.title} xs={12} sm={6} md={4}>
               <Card>
                 <CardHeader
@@ -178,26 +206,8 @@ function App() {
               </Card>
             </Grid>
           ))}
-          <Container className={classes.container} maxWidth="md">
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              autoFocus
-              placeholder="検索商品"
-              value={word}
-              onChange={event => setWord(event.target.value)}
-            />
-            <Button className={classes.button} disabled={loadging} variant="contained" color="primary" onClick={search}>
-              検索する
-            </Button>
-          </Container>
-          <Grid item xs={12}>
-            {message && <Typography align="center" className={classes.textWarning}>{message}</Typography>}
-          </Grid>
         </Grid>
-        <Box mt={5}>
+        <Box className={classes.footer} mt={5}>
           <Typography align="center">
             <Link href="https://twitter.com/memorandumrail">Twitter</Link>{' '}|{' '}
             <Link href="https://memorandumrail.com/contact">お問い合わせ</Link>
